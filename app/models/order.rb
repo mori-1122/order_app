925496f93f8855f67564e22ea227f2c30ec0f292
Chiba <chi.11220214.yo@gmail.com>
2025-12-05 12:36:11 +0900

    emailの正規表現バリデーションを追加し、冗長な記述を整理

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :telephone, presence: true, length: { maximum: 11 }, numericality: { only_integer: true }
+  validates :delivery_address, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'の形式が正しくありません' }
 end
